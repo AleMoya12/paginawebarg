0efb632923abb62f1aa3385a88721ef1728f8ddf
AleMoya12 <alemoya5@gmail.com>
2026-05-29 00:19:51 -0300

    fix: eliminar distDir para vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "dist",
+  
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
